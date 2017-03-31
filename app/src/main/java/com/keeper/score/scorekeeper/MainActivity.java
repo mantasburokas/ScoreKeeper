82b757a009d78f4f5d2dabf4bbe8672159275109
Mantas Burokas <m.burokas@loginvsi.com>
2017-04-01 02:26:06 +0300

    Initial commit

---
+++ app/src/main/java/com/keeper/score/scorekeeper/MainActivity.java
@@ -0,0 +1,13 @@
+package com.keeper.score.scorekeeper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
